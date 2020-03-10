3566d345dffbe56762153917c09731639d1dcf8f
marcelobaldi <luizmarcelobaldi@hotmail.com>
2020-03-10 08:11:22 -0300

    add score and restart button

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int score;
+  final void Function() restartQuiz;
+
+  Result(this.score, this.restartQuiz);
+
+  String get result {
+    if (score < 8) {
+      return "Congratulations!";
+    } else if (score < 12) {
+      return "You are good!";
+    } else if (score < 16) {
+      return "Spectacular!";
+    } else {
+      return "Jedi level!";
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Text("Congratulations!", style: TextStyle(fontSize: 28),),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            result,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        FlatButton(
+          onPressed: restartQuiz,
+          child: Text(
+            "Restart?",
+            style: TextStyle(fontSize: 18),
+          ),
+          textColor: Colors.blue,
+        ),
+      ],
     );
   }
 }
